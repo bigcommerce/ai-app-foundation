251fdfd1e1b56bd89f7ae986bab5b920d75ee918
solofeed <yevhenii.kuzmenko@bigcommerce.com>
2023-06-28 01:08:25 +0300

    add basic wrappers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+
+import { Box, GlobalStyles } from '@bigcommerce/big-design';
+import { theme as defaultTheme } from '@bigcommerce/big-design-theme';
+import { ThemeProvider } from 'styled-components';
+// import SessionProvider from '../context/session';
+
+const MyApp: AppType = ({ Component, pageProps }) =>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyles />
+    <Box
+      marginHorizontal={{ mobile: 'none', tablet: 'xxxLarge' }}
+      marginVertical={{ mobile: 'none', tablet: "xxLarge" }}
+    >
+      {/* <SessionProvider> */}
+      <Component {...pageProps} />
+      {/* </SessionProvider> */}
+    </Box>
+  </ThemeProvider>
+  ;
 
 export default api.withTRPC(MyApp);
