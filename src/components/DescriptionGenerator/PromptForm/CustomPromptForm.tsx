cd353e207b34540d17702098427c40974b81d762
bc-rmalyavc <roman.malyavchick@bigcommerce.com>
2023-07-05 19:57:50 +0300

    chore: DATA-11195 Updates as per design changes

--- src/components/DescriptionGenerator/PromptForm/CustomPromptForm.tsx
+++ src/components/DescriptionGenerator/PromptForm/CustomPromptForm.tsx
@@ -1,16 +1,17 @@
-import { type PromptFormProps } from './types';
-import { Checkbox, FormGroup, Textarea } from '@bigcommerce/big-design';
-import { type ChangeEvent, useState } from 'react';
+import { type BasePromptFormProps } from './types';
+import { Checkbox, CheckboxLabel, FormControlLabel, FormGroup, Textarea } from '@bigcommerce/big-design';
+import React, { type ChangeEvent, useEffect, useState } from 'react';
 import { type CustomPromptAttributes } from '../types';
+import { InputLabel } from '~/components/DescriptionGenerator/PromptForm/InputLabel';
 
 type InputFieldValue = string | boolean | undefined;
 
 const INITIAL_FORM_ATTRIBUTES: CustomPromptAttributes = {
     includeProductAttributes: true,
-    customPrompt: 'Short product description highlighting usage innovative environment-friendly materials. Include material names, tell about pros of each and compare to the most popular ones. Add summary in a last paragraph. Make it sound professional and convincing.',
+    customPrompt: 'Short product description highlighting usage innovative environment-friendly materials. Include material names, tell about props of each and compare to the most popular ones. Add summary in a last paragraph. Make it sound professional and convincing.',
 };
 
-export function CustomPromptForm({ onChange }: PromptFormProps) {
+export function CustomPromptForm({ onChange }: BasePromptFormProps) {
     const [formAttributes, setFormAttributes] = useState(INITIAL_FORM_ATTRIBUTES);
 
     const handleInputChange = (value: InputFieldValue, fieldName: keyof CustomPromptAttributes) => {
@@ -23,11 +24,24 @@ export function CustomPromptForm({ onChange }: PromptFormProps) {
         onChange(updatedAttributes);
     }
 
+    useEffect(() => {
+        onChange(formAttributes);
+    }, [formAttributes, onChange]);
+
     return (
         <>
             <FormGroup>
                 <Textarea
-                    label="Instructions"
+                    label={
+                        <FormControlLabel>
+                            <InputLabel
+                                bold={true}
+                                text="Instructions"
+                                tooltip="Enter your custom instructions for AI to generate the description"
+                                // TODO: Change tooltip copy once the design is ready
+                            />
+                        </FormControlLabel>
+                    }
                     onChange={(event: ChangeEvent<HTMLTextAreaElement>) => handleInputChange(event.target.value, 'customPrompt')}
                     rows={4}
                     value={formAttributes.customPrompt}
@@ -36,7 +50,15 @@ export function CustomPromptForm({ onChange }: PromptFormProps) {
             <FormGroup>
                 <Checkbox
                     checked={formAttributes.includeProductAttributes}
-                    label="Include product information"
+                    label={
+                        <CheckboxLabel>
+                            <InputLabel
+                                bold={false}
+                                text="Include product information"
+                                tooltip="If checked, the description will feature information from the product page for this product. This includes: product name, product type, brand, dimensions and weight, categories and condition."
+                            />
+                        </CheckboxLabel>
+                    }
                     onChange={() => handleInputChange(!formAttributes.includeProductAttributes, 'includeProductAttributes')}
                 />
             </FormGroup>
---
+++ src/components/DescriptionGenerator/PromptForm/styled.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button<{ isActive: boolean }>`
+    align-items: center;
+    appearance: none;
+    background-color: ${({ isActive, theme }) => (isActive ? theme.colors.primary20 : theme.colors.white)};
+    border-top: ${({ theme }) => theme.border.box};
+    border-left: ${({ theme }) => theme.border.box};
+    border-bottom: ${({ theme }) => theme.border.box};
+    border-color: ${({ isActive, theme }) => (isActive ? theme.colors.primary30 : theme.colors.secondary30)};
+    border-right: 0;
+    border-radius: 0;
+    color: ${({ isActive, theme }) => (isActive ? theme.colors.primary60 : theme.colors.secondary60)};
+    cursor: pointer;
+    display: inline-flex;
+    flex: none;
+    font-size: ${({ theme }) => theme.typography.fontSize.medium};
+    font-weight: ${({ theme }) => theme.typography.fontWeight.regular};
+    justify-content: center;
+    line-height: ${({ theme }) => theme.lineHeight.xLarge};
+    outline: none;
+    padding: ${({ theme }) => `0 ${theme.spacing.medium}`};
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    user-select: none;
+    vertical-align: middle;
+    white-space: nowrap;
+    width: auto;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:first-child {
+        border-top-left-radius: ${({ theme }) => theme.borderRadius.normal};
+        border-bottom-left-radius: ${({ theme }) => theme.borderRadius.normal};
+    }
+
+    &:last-child {
+        border-top-right-radius: ${({ theme }) => theme.borderRadius.normal};
+        border-bottom-right-radius: ${({ theme }) => theme.borderRadius.normal};
+        border-right: ${({ theme }) => theme.border.box};
+        border-right-color: ${({ isActive, theme }) => (isActive ? theme.colors.primary30 : theme.colors.secondary30)};
+    }
+
+    ${({ isActive, theme }) =>
+    isActive &&
+    `
+        + button {
+            border-left-color: ${theme.colors.primary30};
+        }
+    `}
+`;
