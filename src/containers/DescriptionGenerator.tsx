5ee495f7b4c3df7b76b0451461324da529981b81
solofeed <yevhenii.kuzmenko@bigcommerce.com>
2023-07-25 01:38:06 +0300

    add DescriptionGenerator container

---
+++ src/containers/DescriptionGenerator.tsx
@@ -0,0 +1,80 @@
+import { Flex, FlexItem, Button, Box } from "@bigcommerce/big-design";
+import { type NewProduct, type Product } from "types";
+import AiResults from "~/components/AiResults/AiResults";
+import Loader from "~/components/Loader";
+import { CustomPromptForm } from "~/components/PromptForm/CustomPromptForm";
+import { StucturedPromptForm } from "~/components/PromptForm/TemplatePromptForm";
+import { StyledButton } from "~/components/PromptForm/styled";
+import { usePromptAttributes } from "~/context/PromptAttributesContext";
+import { useAIDescriptions } from "~/hooks";
+import { prepareAiPromptAttributes } from "~/utils/utils";
+
+interface DescriptonGeneratorProps {
+    product: Product | NewProduct;
+}
+
+export default function DescriptonGenerator({ product }: DescriptonGeneratorProps) {
+    const {
+        isPrompting,
+        results,
+        generateDescription,
+        setResults,
+        handleDescriptionChange
+    } = useAIDescriptions(product.id);
+
+    const {
+        isFormStructured,
+        setIsFormStructured,
+        currentAttributes,
+        structuredAttributes,
+        customAttributes,
+        setStructuredAttributes,
+        setCustomAttributes
+    } = usePromptAttributes();
+
+    const handleGenerateDescription = () => {
+        const input = prepareAiPromptAttributes(currentAttributes, product);
+        void generateDescription(
+            input,
+            { onSuccess: (description) => setResults({ promptAttributes: currentAttributes, description }) }
+        );
+    };
+
+    return (
+        <>
+            <Flex flexDirection="column">
+                <FlexItem>
+                    <Box display="inline-flex" marginBottom="medium">
+                        <StyledButton isActive={isFormStructured} onClick={() => setIsFormStructured(true)}>
+                            Structured prompt
+                        </StyledButton>
+                        <StyledButton isActive={!isFormStructured} onClick={() => setIsFormStructured(false)}>
+                            Custom prompt
+                        </StyledButton>
+                    </Box>
+                    {isFormStructured
+                        ? <StucturedPromptForm attributes={structuredAttributes} onChange={setStructuredAttributes} />
+                        : <CustomPromptForm attributes={customAttributes} onChange={setCustomAttributes} />
+                    }
+                    <FlexItem marginTop="small">
+                        <Button disabled={isPrompting} mobileWidth="auto" variant="secondary" onClick={() => void handleGenerateDescription()}>
+                            Write more
+                        </Button>
+                    </FlexItem>
+                </FlexItem>
+                <FlexItem marginTop="medium">
+                    {isPrompting && <Loader />}
+                    {!isPrompting && <AiResults onChange={handleDescriptionChange} results={results} />}
+                </FlexItem>
+            </Flex>
+            {!isPrompting &&
+                <FlexItem marginTop="xxLarge">
+                    <Flex justifyContent="flex-end" flexDirection="row">
+                        <Button mobileWidth="auto" variant="secondary">Cancel</Button>
+                        <Button mobileWidth="auto" variant="primary">Use this result</Button>
+                    </Flex>
+                </FlexItem>
+            }
+        </>
+    );
+}
