51a5198804f3cf7fe0058a6db5e92dff95dd0272
solofeed <yevhenii.kuzmenko@bigcommerce.com>
2023-07-25 01:35:35 +0300

    refactor AiResulsts and PromptForm

--- src/components/DescriptionGenerator/PromptForm/types.ts
+++
@@ -1,9 +0,0 @@
-import { type PromptAttributes } from '~/components/DescriptionGenerator/types';
-
-export interface BasePromptFormProps {
-    onChange(promptAttributes: PromptAttributes): void;
-}
-
-export interface PromptFormProps extends BasePromptFormProps {
-    generateDescription(): Promise<void>;
-}
--- src/components/DescriptionGenerator/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './DescriptionGenerator';
-export * from './types';
