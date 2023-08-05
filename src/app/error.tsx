bfca368974ba8ae2d328b9bce302848c8bd10d30
solofeed <yevhenii.kuzmenko@bigcommerce.com>
2023-08-05 11:32:08 +0300

    add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Button, FlexItem, H2, Text } from '@bigcommerce/big-design';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <FlexItem>
+      <H2>Something went wrong!</H2>
+      <Text>{error.message}</Text>
+      <Button mobileWidth="auto" onClick={reset}>
+        Try again
+      </Button>
+    </FlexItem>
+  );
+}
